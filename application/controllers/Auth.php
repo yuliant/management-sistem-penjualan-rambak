c9859a3861794d9800eff4f1c4b0cf17b6d5e5fe
Masrizal Eka Yulianto <masrizal04@gmail.com>
2019-05-03 10:02:32 +0700

    Login: sederhana

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -10,11 +10,48 @@ class Auth extends CI_Controller {
 
 	public function index()
 	{
-		$data['title']='Login Page';
-		$this->load->view('templates/auth_header', $data);
-		$this->load->view('auth/login');
-		$this->load->view('templates/auth_footer');
+		$this->form_validation->set_rules("email","Email","required|valid_email|trim");
+		$this->form_validation->set_rules("password","Password","required|trim");
+		if ($this->form_validation->run() == false) {
+			$data['title']='Login Page';
+			$this->load->view('templates/auth_header', $data);
+			$this->load->view('auth/login');
+			$this->load->view('templates/auth_footer');
+		}else {
+			//validation success menggunakan method private
+			$this->_login();
+		}
 	}
+	private function _login(){
+		$email = $this->input->post('email');
+		$password = $this->input->post('password');
+
+		$user = $this->db->get_where('user', ['email'=>$email])->row_array();
+		if ($user) { // jika usernya ada
+			// jika usernya aktif
+			if ($user['is_active'] == 1) {
+				// cek password
+				if (password_verify($password, $user['password'])) {
+					$data = [
+						'email' => $user['email'],
+						'role_id' => $user['role_id']
+					];
+					$this->session->set_userdata($data);
+					redirect('user');
+				}else {
+					$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Wrong password</div>');
+					redirect('auth');
+				}
+			}else {
+				$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">This email has not been activated</div>');
+				redirect('auth');
+			}
+		}else {
+			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email is not registered</div>');
+			redirect('auth');
+		}
+	}
+
 	public function registration()
 	{
 		$this->form_validation->set_rules("name","Name","required|trim");
@@ -34,10 +71,10 @@ class Auth extends CI_Controller {
 			$this->load->view('templates/auth_footer');
 		}else {
 			$data = [
-				'name' => $this->input->post('name'),
+				'name' => htmlspecialchars($this->input->post('name', true)),
 				'email' => htmlspecialchars($this->input->post('email', true)),
 				'image' => 'default.jpg',
-				'password' => password_hash($this->input->post('password'),PASSWORD_DEFAULT),
+				'password' => password_hash($this->input->post('password1'),PASSWORD_DEFAULT),
 				'role_id' => 2,
 				'is_active' => 1,
 				'date_created' => time()
@@ -47,5 +84,13 @@ class Auth extends CI_Controller {
 			redirect('auth');
 		}
 
+	}
+
+	public function logout(){
+		$this->session->unset_userdata('email');
+		$this->session->unset_userdata('role_id');
+
+		$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">You have been logged out</div>');
+		redirect('auth');
 	}
 }
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -18,12 +18,14 @@
 
                   <?php echo $this->session->flashdata('message'); ?>
 
-                  <form class="user">
+                  <form class="user" method="post" action="<?php echo base_url('auth'); ?>">
                     <div class="form-group">
-                      <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Enter Email Address...">
+                      <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Enter Email Address..." value="<?php echo set_value('email'); ?>">
+                      <?php echo form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                     </div>
                     <div class="form-group">
                       <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
+                      <?php echo form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
                     </div>
                     <button type="submit" class="btn btn-primary btn-user btn-block">
                       Login
