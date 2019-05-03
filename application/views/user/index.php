d0bb0006bc884c7aea2056e5e3e3ac3810ef7dd4
Masrizal Eka Yulianto <masrizal04@gmail.com>
2019-05-03 20:05:28 +0700

    User and admin page: sederhana

---
+++ application/views/user/index.php
@@ -0,0 +1,28 @@
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <h1 class="h3 mb-4 text-gray-800"><?php echo $title; ?></h1>
+
+          <!-- Card Profile -->
+          <div class="card mb-3" style="max-width: 540px;">
+            <div class="row no-gutters">
+              <div class="col-md-4">
+                <img src="<?php echo base_url('assets/img/profile/') . $user['image']; ?>" class="card-img" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title"><?php echo $user['name']; ?></h5>
+                  <p class="card-text"><?php echo $user['email']; ?></p>
+                  <p class="card-text"><small class="text-muted">Mamber since <?php echo date('d F Y', $user['date_created']); ?></small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
